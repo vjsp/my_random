1175a4391f279d5a982caa1960d747e514b5269c
vjsp <vj.sanchezp@gmail.com>
2015-07-08 05:36:41 +0200

    random versión 2

--- random.js
+++ random.js
@@ -1,8 +1,11 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
+var str = ' MAYOR que 0,5';
+
 if (numero <= 0.5) {
-  console.log('\n' + numero + ' MENOR que 0,5 \n');
-} else {
-  console.log('\n' + numero + ' MAYOR que 0,5 \n');
+  str = ' MENOR que 0,5';
+}
+
+console.log('\n' + numero + str + '\n');
 }
